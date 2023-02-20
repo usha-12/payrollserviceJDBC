98856d82c1c869305d535808681d1c4a344233df
usha-12 <usha12ahirwar@gmail.com>
2023-02-20 14:01:29 +0530

    Added: Retrive all data from database using prepareStatement

--- src/main/java/com/brideglabz/jdbc/EmployeePayroll.java
+++ src/main/java/com/brideglabz/jdbc/EmployeePayroll.java
@@ -3,17 +3,24 @@ package com.brideglabz.jdbc;
 import java.sql.*;
 
 public class EmployeePayroll {
-    public static void preparedStatement(String name, double salary) {
+    public static void CreatedConnection() {
 
         try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root",
                     "Usha@123");
-            String s = "update employee_payroll set salary = ? where name=?";
+            String s = "select * from employee_payroll";
             PreparedStatement ps = conn.prepareStatement(s);
-            ps.setDouble(1, salary);
-            ps.setString(2, name);
-            ps.executeUpdate();
-            boolean result = true;
+            ResultSet result = ps.executeQuery();
+
+            while (result.next()) {
+                int id = result.getInt(1);
+                String name = result.getString(2);
+                String gender = result.getString(3);
+                Double salary = result.getDouble(4);
+                String date = result.getString(5);
+                System.out.println(
+                        "id:- " + id + " Name:- " + name + " Gender:- " + " | " + " Salary:- " + salary + " Date:- " + date);
+            }
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -22,7 +29,8 @@ public class EmployeePayroll {
     }
 
     public static void main(String[] args) throws SQLException {
-        preparedStatement(" LESA",  300000.000);
+        CreatedConnection();
 
     }
+
 }
